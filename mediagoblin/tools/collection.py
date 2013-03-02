ab44861b800f9c16765423e652ed24d7a7b54e86
MusiqueLibre <dev@dogmazic.net>
2013-03-02 21:08:50 +0100

    correcting media collect tools after the merge

--- mediagoblin/tools/collection.py
+++ mediagoblin/tools/collection.py
@@ -4,67 +4,56 @@ from mediagoblin.tools.response import redirect
 from mediagoblin.tools.translate import pass_to_ugettext as _
 
 def collection_tools(request, media, collection_form, new_media=False):
-
-    if request.method != 'POST' or not collection_form.validate():
-        # No POST submission, or invalid form
-        if not form.validate():
-            messages.add_message(request, messages.ERROR,
-                _('Please check your entries and try again.'))
-
-        return render_to_response(
-            request,
-            'mediagoblin/user_pages/media_collect.html',
-            {'media': media,
-             'collection_form': collection_form})
-
     # If we are here, method=POST and the form is valid, submit things.
     # If the user is adding a new collection, use that:
-    if request.form['collection_title'] != '':
+    if request.collection_form['collection_title']:
         # Make sure this user isn't duplicating an existing collection
         existing_collection = Collection.query.filter_by(
                                 creator=request.user.id,
-                                title=request.form['collection_title']).first()
-        #If someone types an existing collection, use that collection
+                                title=request.collection_form['collection_title']).first()
         if existing_collection:
-            collection = existing_collection
-        else:
-            collection = Collection()
-            collection.title = request.form['collection_title']
-            collection.description = request.form.get('collection_description')
-            collection.creator = request.user.id
-            collection.generate_slug()
-            collection.save()
+            messages.add_message(request, messages.ERROR,
+                _('You already have a collection called "%s"!')
+                % existing_collection.title)
+            return redirect(request, "mediagoblin.user_pages.media_home",
+                            user=media.get_uploader.username,
+                            media=media.slug_or_id)
+
+        collection = Collection()
+        collection.title = request.collection_form['collection_title']
+        collection.description = request.collection_form.get('collection_description')
+        collection.creator = request.user.id
+        collection.generate_slug()
+        collection.save()
 
     # Otherwise, use the collection selected from the drop-down
-    elif request.form.get('collection') != '__None':
+    else:
         collection = Collection.query.filter_by(
-            id=request.form.get('collection')).first()
-    elif new_media == False:
+            id=request.collection_form.get('collection')).first()
+
     # Make sure the user actually selected a collection
+    if not collection:
         messages.add_message(
             request, messages.ERROR,
             _('You have to select or add a collection'))
-        return redirect(request, "mediagoblin.user_pages.media_home",
+        return redirect(request, "mediagoblin.user_pages.media_collect",
                     user=media.get_uploader.username,
                     media=media.id)
-   #if there's no collection and it's a new media just do nothing
-    else:
-        return
-       
+
 
     # Check whether media already exists in collection
-    if CollectionItem.query.filter_by(
+    elif CollectionItem.query.filter_by(
         media_entry=media.id,
         collection=collection.id).first():
         messages.add_message(request, messages.ERROR,
-            _('"%s" already in collection "%s"'
-                % (media.title, collection.title)))
+                             _('"%s" already in collection "%s"')
+                             % (media.title, collection.title))
     else: # Add item to collection
         collection_item = request.db.CollectionItem()
         collection_item.collection = collection.id
         collection_item.media_entry = media.id
         collection_item.author = request.user.id
-        collection_item.note = request.form['note']
+        collection_item.note = request.collection_form['note']
         collection_item.save()
 
         collection.items = collection.items + 1
@@ -74,9 +63,9 @@ def collection_tools(request, media, collection_form, new_media=False):
         media.save()
 
         messages.add_message(request, messages.SUCCESS,
-                             _('"%s" added to collection "%s"'
-                               % (media.title, collection.title)))
+                             _('"%s" added to collection "%s"')
+                             % (media.title, collection.title))
 
     return redirect(request, "mediagoblin.user_pages.media_home",
                     user=media.get_uploader.username,
-                    media=media.id)
+                    media=media.slug_or_id)
